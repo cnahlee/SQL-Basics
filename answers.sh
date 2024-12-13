ead28459c1fdf06073b8fb2d892c64291840d926
cnahlee <cynthia.nahle@isae.edu.lb>
2024-12-13 21:41:57 +0200

    creating table

--- answers.sh
+++ answers.sh
@@ -19,3 +19,24 @@ WHERE Name = 'Basma';
 UPDATE Students
 SET Points = Points - 5
 WHERE Name = 'Alex';
+
+CREATE TABLE graduates (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    Graduation TEXT
+);
+
+INSERT INTO graduates (Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points
+FROM students
+WHERE Name = 'Layal';
+
+UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE Name = 'Layal';
+
+DELETE FROM students
+WHERE Name = 'Layal';
